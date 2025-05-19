99b82b1b22a90e1ddf7b6713ec4b931d0a2fca85
sbelambe <sbelambe@ucsc.edu>
2025-05-19 16:33:31 -0700

    layersToGrid

--- z3-web-browser-copy/src/Scenes/Pathfinder.js
+++ z3-web-browser-copy/src/Scenes/Pathfinder.js
@@ -1,168 +1,217 @@
 import Phaser from "phaser";
 export default class Pathfinder extends Phaser.Scene {
-    constructor() {
-        super("pathfinderScene");
-    }
+  constructor() {
+    super("pathfinderScene");
+  }
 
-    preload() {
-    }
+  preload() {}
 
-    init() {
-        this.TILESIZE = 16;
-        this.SCALE = 2.0;
-        this.TILEWIDTH = 40;
-        this.TILEHEIGHT = 25;
-    }
+  init() {
+    this.TILESIZE = 16;
+    this.SCALE = 2.0;
+    this.TILEWIDTH = 40;
+    this.TILEHEIGHT = 25;
+  }
 
-    create() {
-        // Create a new tilemap which uses 16x16 tiles, and is 40 tiles wide and 25 tiles tall
-        this.map = this.add.tilemap("three-farmhouses", this.TILESIZE, this.TILESIZE, this.TILEHEIGHT, this.TILEWIDTH);
+  create() {
+    // Create a new tilemap which uses 16x16 tiles, and is 40 tiles wide and 25 tiles tall
+    this.map = this.add.tilemap(
+      "three-farmhouses",
+      this.TILESIZE,
+      this.TILESIZE,
+      this.TILEHEIGHT,
+      this.TILEWIDTH
+    );
 
-        // Add a tileset to the map
-        this.tileset = this.map.addTilesetImage("kenney-tiny-town", "tilemap_tiles");
+    // Add a tileset to the map
+    this.tileset = this.map.addTilesetImage(
+      "kenney-tiny-town",
+      "tilemap_tiles"
+    );
 
-        // Create the layers
-        this.groundLayer = this.map.createLayer("Ground-n-Walkways", this.tileset, 0, 0);
-        this.treesLayer = this.map.createLayer("Trees-n-Bushes", this.tileset, 0, 0);
-        this.housesLayer = this.map.createLayer("Houses-n-Fences", this.tileset, 0, 0);
+    // Create the layers
+    this.groundLayer = this.map.createLayer(
+      "Ground-n-Walkways",
+      this.tileset,
+      0,
+      0
+    );
+    this.treesLayer = this.map.createLayer(
+      "Trees-n-Bushes",
+      this.tileset,
+      0,
+      0
+    );
+    this.housesLayer = this.map.createLayer(
+      "Houses-n-Fences",
+      this.tileset,
+      0,
+      0
+    );
 
-        // Create townsfolk sprite
-        // Use setOrigin() to ensure the tile space computations work well
-        this.purpleTownie = this.add.sprite(this.tileXtoWorld(5), this.tileYtoWorld(5), "purple").setOrigin(0,0);
-        
-        // Camera settings
-        this.cameras.main.setBounds(0, 0, this.map.widthInPixels, this.map.heightInPixels);
-        this.cameras.main.setZoom(this.SCALE);
+    // Create townsfolk sprite
+    // Use setOrigin() to ensure the tile space computations work well
+    this.purpleTownie = this.add
+      .sprite(this.tileXtoWorld(5), this.tileYtoWorld(5), "purple")
+      .setOrigin(0, 0);
 
-        // Create grid of visible tiles for use with path planning
-        let tinyTownGrid = this.layersToGrid([this.groundLayer, this.treesLayer, this.housesLayer]);
+    // Camera settings
+    this.cameras.main.setBounds(
+      0,
+      0,
+      this.map.widthInPixels,
+      this.map.heightInPixels
+    );
+    this.cameras.main.setZoom(this.SCALE);
 
-        let walkables = [1, 2, 3, 30, 40, 41, 42, 43, 44, 95, 13, 14, 15, 25, 26, 27, 37, 38, 39, 70, 84];
+    // Create grid of visible tiles for use with path planning
+    let tinyTownGrid = this.layersToGrid([
+      this.groundLayer,
+      this.treesLayer,
+      this.housesLayer,
+    ]);
 
-        // Initialize EasyStar pathfinder
-        this.finder = new EasyStar.js();
+    let walkables = [
+      1, 2, 3, 30, 40, 41, 42, 43, 44, 95, 13, 14, 15, 25, 26, 27, 37, 38, 39,
+      70, 84,
+    ];
 
-        // Pass grid information to EasyStar
-        // EasyStar doesn't natively understand what is currently on-screen,
-        // so, you need to provide it that information
-        this.finder.setGrid(tinyTownGrid);
+    // Initialize EasyStar pathfinder
+    this.finder = new EasyStar.js();
 
-        // Tell EasyStar which tiles can be walked on
-        this.finder.setAcceptableTiles(walkables);
+    // Pass grid information to EasyStar
+    // EasyStar doesn't natively understand what is currently on-screen,
+    // so, you need to provide it that information
+    this.finder.setGrid(tinyTownGrid);
 
-        this.activeCharacter = this.purpleTownie;
+    // Tell EasyStar which tiles can be walked on
+    this.finder.setAcceptableTiles(walkables);
 
-        // Handle mouse clicks
-        // Handles the clicks on the map to make the character move
-        // The this parameter passes the current "this" context to the
-        // function this.handleClick()
-        this.input.on('pointerup',this.handleClick, this);
+    this.activeCharacter = this.purpleTownie;
 
-        this.cKey = this.input.keyboard.addKey('C');
+    // Handle mouse clicks
+    // Handles the clicks on the map to make the character move
+    // The this parameter passes the current "this" context to the
+    // function this.handleClick()
+    this.input.on("pointerup", this.handleClick, this);
+
+    this.cKey = this.input.keyboard.addKey("C");
+    this.lowCost = false;
+  }
+
+  update() {
+    if (Phaser.Input.Keyboard.JustDown(this.cKey)) {
+      if (!this.lowCost) {
+        // Make the path low cost with respect to grassy areas
+        this.setCost(this.tileset);
+        this.lowCost = true;
+      } else {
+        // Restore everything to same cost
+        this.resetCost(this.tileset);
         this.lowCost = false;
-
+      }
     }
+  }
 
-    update() {
-        if (Phaser.Input.Keyboard.JustDown(this.cKey)) {
-            if (!this.lowCost) {
-                // Make the path low cost with respect to grassy areas
-                this.setCost(this.tileset);
-                this.lowCost = true;
-            } else {
-                // Restore everything to same cost
-                this.resetCost(this.tileset);
-                this.lowCost = false;
-            }
+  resetCost(tileset) {
+    for (let tileID = tileset.firstgid; tileID < tileset.total; tileID++) {
+      let props = tileset.getTileProperties(tileID);
+      if (props != null) {
+        if (props.cost != null) {
+          this.finder.setTileCost(tileID, 1);
         }
+      }
     }
+  }
 
-    resetCost(tileset) {
-        for (let tileID = tileset.firstgid; tileID < tileset.total; tileID++) {
-            let props = tileset.getTileProperties(tileID);
-            if (props != null) {
-                if (props.cost != null) {
-                    this.finder.setTileCost(tileID, 1);
-                }
-            }
+  tileXtoWorld(tileX) {
+    return tileX * this.TILESIZE;
+  }
+
+  tileYtoWorld(tileY) {
+    return tileY * this.TILESIZE;
+  }
+
+  // layersToGrid
+  //
+  // Uses the tile layer information in this.map and outputs
+  // an array which contains the tile ids of the visible tiles on screen.
+  // This array can then be given to Easystar for use in path finding.
+  layersToGrid() {
+    let grid = [];
+    // Initialize grid as two-dimensional array
+    // TODO: write initialization code
+
+    // Loop over layers to find tile IDs, store in grid
+    // TODO: write this loop
+    for (let i = 0; i < this.TILEHEIGHT; i++) {
+      let row = [];
+      for (let j = 0; j < this.TILEWIDTH; j++) {
+        const tile = this.groundLayer.getTileAt(j, i);
+        const treesTile = this.treesLayer.getTileAt(x, y);
+        const housesTile = this.housesLayer.getTileAt(x, y);
+        if (treesTile || housesTile) {
+          row.push(-1); // Blocked tile
+        } else if (tile) {
+          row.push(tile.index); // Ground tile index
+        } else {
+          row.push(0); // Empty fallback
         }
+      }
+      grid.push(row);
     }
 
-    tileXtoWorld(tileX) {
-        return tileX * this.TILESIZE;
+    return grid;
+  }
+
+  handleClick(pointer) {
+    let x = pointer.x / this.SCALE;
+    let y = pointer.y / this.SCALE;
+    let toX = Math.floor(x / this.TILESIZE);
+    var toY = Math.floor(y / this.TILESIZE);
+    var fromX = Math.floor(this.activeCharacter.x / this.TILESIZE);
+    var fromY = Math.floor(this.activeCharacter.y / this.TILESIZE);
+    console.log(
+      "going from (" + fromX + "," + fromY + ") to (" + toX + "," + toY + ")"
+    );
+
+    this.finder.findPath(fromX, fromY, toX, toY, (path) => {
+      if (path === null) {
+        console.warn("Path was not found.");
+      } else {
+        console.log(path);
+        this.moveCharacter(path, this.activeCharacter);
+      }
+    });
+    this.finder.calculate(); // ask EasyStar to compute the path
+    // When the path computing is done, the arrow function given with
+    // this.finder.findPath() will be called.
+  }
+
+  moveCharacter(path, character) {
+    // Sets up a list of tweens, one for each tile to walk, that will be chained by the timeline
+    var tweens = [];
+    for (var i = 0; i < path.length - 1; i++) {
+      var ex = path[i + 1].x;
+      var ey = path[i + 1].y;
+      tweens.push({
+        x: ex * this.map.tileWidth,
+        y: ey * this.map.tileHeight,
+        duration: 200,
+      });
     }
 
-    tileYtoWorld(tileY) {
-        return tileY * this.TILESIZE;
-    }
+    this.tweens.chain({
+      targets: character,
+      tweens: tweens,
+    });
+  }
 
-    // layersToGrid
-    //
-    // Uses the tile layer information in this.map and outputs
-    // an array which contains the tile ids of the visible tiles on screen.
-    // This array can then be given to Easystar for use in path finding.
-    layersToGrid() {
-        let grid = [];
-        // Initialize grid as two-dimensional array
-        // TODO: write initialization code
-
-        // Loop over layers to find tile IDs, store in grid
-        // TODO: write this loop
-
-        return grid;
-    }
-
-
-    handleClick(pointer) {
-        let x = pointer.x / this.SCALE;
-        let y = pointer.y / this.SCALE;
-        let toX = Math.floor(x/this.TILESIZE);
-        var toY = Math.floor(y/this.TILESIZE);
-        var fromX = Math.floor(this.activeCharacter.x/this.TILESIZE);
-        var fromY = Math.floor(this.activeCharacter.y/this.TILESIZE);
-        console.log('going from ('+fromX+','+fromY+') to ('+toX+','+toY+')');
-    
-        this.finder.findPath(fromX, fromY, toX, toY, (path) => {
-            if (path === null) {
-                console.warn("Path was not found.");
-            } else {
-                console.log(path);
-                this.moveCharacter(path, this.activeCharacter);
-            }
-        });
-        this.finder.calculate(); // ask EasyStar to compute the path
-        // When the path computing is done, the arrow function given with
-        // this.finder.findPath() will be called.
-    }
-    
-    moveCharacter(path, character) {
-        // Sets up a list of tweens, one for each tile to walk, that will be chained by the timeline
-        var tweens = [];
-        for(var i = 0; i < path.length-1; i++){
-            var ex = path[i+1].x;
-            var ey = path[i+1].y;
-            tweens.push({
-                x: ex*this.map.tileWidth,
-                y: ey*this.map.tileHeight,
-                duration: 200
-            });
-        }
-    
-        this.tweens.chain({
-            targets: character,
-            tweens: tweens
-        });
-
-    }
-
-    // A function which takes as input a tileset and then iterates through all
-    // of the tiles in the tileset to retrieve the cost property, and then 
-    // uses the value of the cost property to inform EasyStar, using EasyStar's
-    // setTileCost(tileID, tileCost) function.
-    setCost(tileset) {
-        // TODO: write this function
-    }
-
-
-
+  // A function which takes as input a tileset and then iterates through all
+  // of the tiles in the tileset to retrieve the cost property, and then
+  // uses the value of the cost property to inform EasyStar, using EasyStar's
+  // setTileCost(tileID, tileCost) function.
+  setCost(tileset) {
+    // TODO: write this function
+  }
 }
